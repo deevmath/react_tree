982ffa64b8e6b7c91be4d501c19cc50f0b34e30f
deevmath <matheus.httsp@gmail.com>
2023-04-04 22:24:04 -0300

    🎨 style(Cards.tsx): add font-sans class to parent div and change text color to text-slate-500 🎨 style(Header.tsx): add border to ImageHeader and change border color to border-indigo-600 🎨 style(global.css): change background color to bg-zinc-900 from bg-slate-900 and text color to text-slate-100

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,11 @@ import ImageHeader from '../img/ImageHeader.jpg'
 export function Header() {
   return (
     <div className=" mt-80 h-full w-full flex flex-col justify-center items-center">
-      <img src={ImageHeader} alt="" className=" w-32 h-32 rounded-full mt-8" />
+      <img
+        src={ImageHeader}
+        alt=""
+        className=" w-32 h-32 rounded-full mt-8 border border-indigo-600"
+      />
     </div>
   )
 }
